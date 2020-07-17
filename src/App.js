da751133abec06eda965c9ffce3d10dfbf228ee7
asharma414 <abhi.sharm.414@gmail.com>
2020-07-16 23:53:29 -0400

    user setup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,52 @@ import ShowPage from './components/ShowPage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import TopNav from './components/TopNav'
 import SideNav from './components/SideNav'
+import Uploader from './components/Uploader'
+import Page404 from './components/404Page'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import {Row, Col} from 'react-bootstrap'
 import './App.css';
 
-function App() {
-  
+class App extends React.Component {
+
+
+  state = {
+    currentUser: null
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("jwt")) {
+      fetch("http://localhost:3000/token", {
+        method: "GET",
+        headers: {
+          "Authentication": localStorage.getItem("jwt")
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          this.updateUser(data)
+        })
+    }
+    //check if there is token in localStorage
+    //if so, make fetch call to find out logged in user
+    //then this.setState of currentUser
+  }
+
+  logoutUser = () => {
+    localStorage.removeItem('jwt')
+    this.setState({ currentUser: null })
+  }
+
+  updateUser = (user) => {
+    this.setState({ currentUser: user })
+  }
+
+  render() {
   return (
     <div className='main'>
     <Row>
       <Col>
-    <TopNav />
+    <TopNav loginUser={this.loginUser} registerUser={this.registerUser} logoutUser={this.logoutUser} />
       </Col>
     </Row>
     <Row>
@@ -25,12 +60,15 @@ function App() {
       <br />
       <Switch>
         <Route exact path='/videos/:id' render={() => <ShowPage />} />
+        <Route exact path='/upload' render={() => <Uploader />} />
+        <Route render={() => <Page404 />} />
       </Switch>
     </Router>
       </Col>
     </Row>
     </div>
   );
+  }
 }
 
 export default App;
--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import DropzoneComponent from 'react-dropzone-component';
 
 export default class Uploader extends Component {
     render() {
+        var componentConfig = { postUrl: 'no-url' };
+        var djsConfig = { autoProcessQueue: false }
+        var eventHandlers = { addedfile: (file) => console.log(file) }
         return (
-            <div>
-                
-            </div>
+            <DropzoneComponent config={componentConfig}
+                eventHandlers={eventHandlers}
+                djsConfig={djsConfig} />
         )
     }
 }
